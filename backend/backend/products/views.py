4482475baf4fb4590e263e5e054f9f49c4d3c43d
AlvinSetyaPranata <comand.alvin@gmail.com>
2024-05-02 08:46:28 +0700

    configuring rest framework in settings

--- backend/backend/products/views.py
+++ backend/backend/products/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
-
+from rest_framework.views import View
+from rest_framework.response import Response
+from products.models import (
+    Product
+)
 # Create your views here.
+
+
+class GetProduct(View):
+    def get(self, _):
+
+        data = Product.objects.all()
+
+        return Response(data)
