83c005f1e81331a0eeea0d85f642f3915f909f39
AlvinSetyaPranata <comand.alvin@gmail.com>
2024-05-02 08:37:17 +0700

    added product model into backend

--- backend/backend/products/models.py
+++ backend/backend/products/models.py
@@ -5,10 +5,15 @@ class ProductCatagory(models.Model):
     name = models.CharField(max_length=50)
 
 
-
 class Product(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
     price = models.IntegerField()
     quantity = models.IntegerField()
-    
+    catagory = models.ForeignKey(ProductCatagory, on_delete=models.CASCADE)
+    date_product = models.DateTimeField(auto_now_add=True, editable=False)
+
+
+class SpecialProduct(models.Model):
+    title = models.CharField(max_length=255)
+    product_name = models.ForeignKey(Product, on_delete=models.CASCADE)
