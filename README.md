c6e13b41936c5f8e77bae35f0161b120d1b6030a
AlvinSetyaPranata <comand.alvin@gmail.com>
2024-05-02 08:29:49 +0700

    initial backend test

---
+++ README.md
@@ -0,0 +1,14 @@
+<h2 align="center">Piring Terbang</h2>
+
+<h3 align="center">Pesan makanan hanya dengan sekali klik</h3>
+
+
+## Teknologi yang digunakan
+- Android: React Native
+- IOS: N/A
+- Backend: Django
+- Frontend Website: N/A
+
+**Tunggu apalagi?, segera download sekarang, Te-he ;p**
+
+<br>
---
+++ backend/backend/products/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+class ProductCatagory(models.Model):
+    name = models.CharField(max_length=50)
+
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.IntegerField()
+    quantity = models.IntegerField()
+    
