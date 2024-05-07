2a084be545b2b8aa86cc89282e9fe7a56f6e0589
AlvinSetyaPranata <comand.alvin@gmail.com>
2024-05-08 04:13:33 +0700

    menambah serializer dan view pada app user

--- backend/backend/users/views.py
+++ backend/backend/users/views.py
@@ -1,3 +1,37 @@
 from django.shortcuts import render
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.permissions import (
+    IsAdminUser,
+    IsAuthenticated
+)
+from rest_framework.response import Response
 
-# Create your views here.
+from .serializers import (
+    UserSerializer
+)
+from .models import (
+    User
+)
+
+class UsersView(APIView):
+    permission_classes = [IsAdminUser]
+
+
+    def get(self, _):
+        users = UserSerializer(User.objects.all())
+
+        return Response(users)
+    
+
+class UserView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        # Do JWT auth
+
+        user = User.objects.filter(name=request.POST["name"])
+
+        user_data = UserSerializer(user)
+
+        return 
