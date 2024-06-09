b9e84cff07fb9a09974dd20e197266024caaf3de
AlvinSetyaPranata <comand.alvin@gmail.com>
2024-06-09 21:01:32 +0700

    Re-initiated web

---
+++ platforms/web/src/components/Navbar.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <div className='flex items-center justify-between px-2'>
+      
+      <div className='flex'>
+        <img src="" alt="logo" />
+        <h1>PiringTerbang</h1>
+      </div>
+
+      <div>
+        <p>Beranda</p>
+        <p>Baru</p>
+        <p>Diskon</p>
+      </div>
+
+
+    </div>
+  )
+}
