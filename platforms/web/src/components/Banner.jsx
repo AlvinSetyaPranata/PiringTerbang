dfaf15422f83b5c83530738caa80331fef3e1015
AlvinSetyaPranata <comand.alvin@gmail.com>
2024-06-13 04:17:46 +0700

    Fix banner animation

--- platforms/web/src/components/Banner.jsx
+++ platforms/web/src/components/Banner.jsx
@@ -1,47 +1,120 @@
 "use client";
 
-import { AnimatePresence, easeIn, easeOut, motion } from "framer-motion";
+import { AnimatePresence, delay, easeIn, easeOut, motion } from "framer-motion";
 // import Image from "next/image";
 import React, { useEffect, useState } from "react";
 
 export default function Banner() {
   const [activeSlide, setActiveSlide] = useState(0);
-  const src = [
-    "https://d1vbn70lmn1nqe.cloudfront.net/prod/wp-content/uploads/2023/07/25033025/resep-masakan-sehari-hari-yang-mudah-dan-anti-bosan-halodoc.jpg.webp",
-    "https://akcdn.detik.net.id/visual/2022/06/07/adv-rankpillar_169.jpeg?w=800&q=90",
-    "https://c4.wallpaperflare.com/wallpaper/303/793/304/buffet-meal-food-brunch-wallpaper-thumb.jpg"
-  ];
 
+  const src = [
+    {
+      title: "Sejuta menu di satu tempat",
+      desc: "Jangan nunggu dia, langsung aja pesan",
+      image:
+        "https://d1vbn70lmn1nqe.cloudfront.net/prod/wp-content/uploads/2023/07/25033025/resep-masakan-sehari-hari-yang-mudah-dan-anti-bosan-halodoc.jpg.webp",
+    },
+    {
+      title: "Menu baru!, masakan padang",
+      desc: "Masakan langsung di buat oleh ahlinya",
+      image:
+        "https://akcdn.detik.net.id/visual/2022/06/07/adv-rankpillar_169.jpeg?w=800&q=90",
+    },
+    {
+      title: "Sejuta menu di satu tempat",
+      desc: "Jangan nunggu dia, langsung aja pesan",
+      image:
+      "https://c4.wallpaperflare.com/wallpaper/303/793/304/buffet-meal-food-brunch-wallpaper-thumb.jpg",
+    },
+  ];
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setActiveSlide(state => (state + 1) % src.length)
+      setActiveSlide((state) => (state + 1) % src.length);
 
-      return () => clearInterval(interval)
-    }, 8000);    
-  }, [])
+      return () => clearInterval(interval);
+    }, 8000);
+  }, []);
+
+  const bannerTitleVariants = {
+    initial: {
+      x: 20,
+      opacity: 0,
+      transition: {
+        duration: 1,
+      },
+    },
+
+    animate: {
+      x: 0,
+      opacity: 1,
+      transition: {
+        duration: 1,
+        delay: 0.5
+      },
+    },
+
+  };
+
+  const bannerDescVariants = {
+    initial: {
+      y: 10,
+      opacity: 0,
+      transition: {
+        duration: 1,
+      },
+    },
+
+    animate: {
+      y: 0,
+      opacity: 1,
+      transition: {
+        duration: 1,
+        delay: 0.5,
+      },
+    },
+  };
 
   return (
     <div className="w-full px-8 text-white min-h-[60vh] flex items-center overflow-hidden relative">
       <div className="absolute min-w-full min-h-full -z-10 left-0 bg-black bg-opacity-75"></div>
       <AnimatePresence>
         <motion.img
-          key={src[activeSlide]}
-          src={src[activeSlide]}
+          key={src[activeSlide].image}
+          src={src[activeSlide].image}
           alt="image"
           className="absolute right-0 -z-20 w-full min-h-[75vh]"
-          exit={{ opacity: 0, transition: {duration: 1.5, ease: easeOut} }}
-          initial={{ opacity: 0, transition: {duration: 1.5, ease: easeOut} }}
-          animate={{ opacity: 1, transition: {duration: 1.5, ease: easeIn, delay: 0.5} }}
+          exit={{ opacity: 0, transition: { duration: 1.5, ease: easeOut } }}
+          initial={{ opacity: 0, transition: { duration: 1.5, ease: easeOut } }}
+          animate={{
+            opacity: 1,
+            transition: { duration: 1.5, ease: easeIn, delay: 0.5 },
+          }}
         />
       </AnimatePresence>
       <div className="absolute min-w-full min-h-full -z-30 left-0 bg-black"></div>
 
       <div>
-        <h1 className="text-5xl font-bold">Masakan padang baru!</h1>
-        <p className="text-gray-300 mt-3 max-w-[50ch]">
-          Memperkenalkan masakan pedas yang dapat membuat lidah anda bergoyang
-        </p>
+        <motion.h1
+          className="text-5xl font-bold"
+          variants={bannerTitleVariants}
+          initial="initial"
+          animate="animate"
+          exit="initial"
+          key={src[activeSlide].title}
+        >
+          {src[activeSlide].title}
+        </motion.h1>
+        <motion.p
+          variants={bannerDescVariants}
+          initial="initial"
+          animate="animate"
+          exit="initial"
+          className="text-gray-300 mt-3 max-w-[50ch]"
+          key={src[activeSlide].desc}
+        >
+          {src[activeSlide].desc}
+        </motion.p>
         <button className="px-4 py-3 mt-6 bg-red-600 text-white rounded-md">
           Pesan sekarang
         </button>
